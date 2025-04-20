d6295bac7dd075c5f3f456a169443adb625d4098
Tomi Scattini <tomiscattini@Mac.glc>
2025-04-20 10:42:25 -0300

    Initial setup

---
+++ loops/main.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function renderListItem(character) {
+    if (!character) return;
+
+    let listElement = document.querySelector(".main-list");
+
+    if (!listElement) {
+      listElement = document.createElement("ul");
+      listElement.classList.add("main-list");
+      document.body.appendChild(listElement);
+    }
+
+    const listItemElement = document.createElement("li");
+
+    const characterImageElement = document.createElement("img");
+    characterImageElement.setAttribute("src", character.image);
+    characterImageElement.setAttribute("alt", character.name);
+    listItemElement.appendChild(characterImageElement);
+
+    const characterNameElement = document.createElement("h2");
+    characterNameElement.innerText = character.name || "";
+    listItemElement.appendChild(characterNameElement);
+
+    listElement.appendChild(listItemElement);
+  }
+
+  async function getRMCharacters() {
+    const request = await fetch("https://rickandmortyapi.com/api/character");
+    const characters = (await request.json())?.results;
+    return characters || [];
+  }
+
+  getRMCharacters().then((characters) => {
+    console.log(characters);
+  });
+});
