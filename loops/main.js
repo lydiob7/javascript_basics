1f388635e2f204421fcca117367083cdbf18502a
Tomi Scattini <tomiscattini@Mac.glc>
2025-04-26 16:37:11 -0300

    Update: add comments

--- loops/main.js
+++ loops/main.js
@@ -33,38 +33,64 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   /* =============== Ejemplos de loops ===================== */
+  // Ir comentando los ejemplos que no se usan para ir viendo los resultados de cada uno
   getRMCharacters().then((characters) => {
-    // for (let i = 0; i < characters.length; i++) {
-    //   renderListItem(characters[i]);
-    // }
-    // let i = 0;
-    // while (i < characters.length) {
-    //   renderListItem(characters[i]);
-    //   i++;
-    // }
-    // let i = 0;
-    // do {
-    //   renderListItem(characters[i]);
-    //   i++;
-    // } while (i < characters.length);
-    // for (const character of characters) {
-    //   renderListItem(character);
-    // }
-    // characters.forEach((character) => {
-    //   renderListItem(character);
-    // });
-    // const rickCharacters = characters.filter((character) => {
-    //   if (character.name.toLowerCase().includes("rick")) return true;
-    //   return false;
-    // });
-    // rickCharacters.forEach((character) => renderListItem(character));
-    // const updatedCharacters = characters.map((character) => {
-    //   console.log(character);
-    //   return {
-    //     ...character,
-    //     name: `Name: ${character.name} - Species: ${character.species}`,
-    //   };
-    // });
-    // updatedCharacters.forEach((character) => renderListItem(character));
+    // For loop
+    for (let i = 0; i < characters.length; i++) {
+      renderListItem(characters[i]);
+    }
+
+    // For loop con palabras clave "continue" y "break"
+    for (let i = 0; i < characters.length; i++) {
+      if (characters[i].name.toLowerCase().includes("jerry")) continue;
+      if (characters[i].name.toLowerCase().includes("alan")) break;
+      renderListItem(characters[i]);
+    }
+
+    // while loop
+    let i = 0;
+    while (i < characters.length) {
+      renderListItem(characters[i]);
+      i++;
+    }
+
+    // do ... while loop
+    let j = 0;
+    do {
+      renderListItem(characters[j]);
+      j++;
+    } while (j < characters.length);
+
+    // for ... of loop
+    for (const character of characters) {
+      renderListItem(character);
+
+      // for ... in loop (para objetos)
+      for (const key in character) {
+        console.log(`${key} >>> ${character[key]}`);
+      }
+    }
+
+    // Método forEach() para arrays
+    characters.forEach((character) => {
+      renderListItem(character);
+    });
+
+    // Método filter() para arrays
+    const rickCharacters = characters.filter((character) => {
+      if (character.name.toLowerCase().includes("rick")) return true;
+      return false;
+    });
+    rickCharacters.forEach((character) => renderListItem(character));
+
+    // método map() para arrays
+    const updatedCharacters = characters.map((character) => {
+      console.log(character);
+      return {
+        ...character,
+        name: `Name: ${character.name} - Species: ${character.species}`,
+      };
+    });
+    updatedCharacters.forEach((character) => renderListItem(character));
   });
 });
