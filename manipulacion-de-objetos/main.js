dcf284770fc46d366846e52b3efa78d8245a978f
Tomi Scattini <tomiscattini@Mac.glc>
2025-04-26 22:35:20 -0300

    Update styles and comments for object manipulation

--- manipulacion-de-objetos/main.js
+++ manipulacion-de-objetos/main.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Renderizar cadena de texto en el HTML
+  function renderText(text) {
+    const containerEl = document.querySelector("#container");
+
+    const newElement = document.createElement("div");
+    newElement.innerText = text;
+    containerEl.appendChild(newElement);
+  }
+
+  // Hacer la peticion a la API de un personaje
   async function getRMCharacter(id = "1") {
     const request = await fetch(
       `https://rickandmortyapi.com/api/character/${id}`,
@@ -7,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     return characters || null;
   }
 
+  // Funcion principal
   getRMCharacter(15).then((character) => {
     console.log(character);
   });
