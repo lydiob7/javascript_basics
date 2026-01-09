4bc89f976ceb4b85a7e2ff2968f5d6a4591aa202
Tomi Scattini <tomiscattini@Tomis-Mac-mini.local>
2026-01-09 09:03:16 +0100

    Feature: add data-types folder

---
+++ data-types/main.js
@@ -0,0 +1,17 @@
+/* ======================= Datos primitivos ========================= */
+
+const myNumber = 1;
+const decimalNumber = 0.8419481;
+
+const myString = "Texto plano";
+
+const myBoolean = true;
+
+const myNull = null;
+
+const myUndefined = undefined;
+
+const stringWithSingleQuotes = "Se pueden usar comillas simples";
+const combinedQuotes =
+  "Puedo usar comillas simples ' dentro de comillas dobles y al reves. También puedo usar \" escapando con una barra invertida";
+const anotherString = `También "backticks", esto permite user variables dentro de la cadena de texto como: ${myString}`;
---
+++ variables/main.js
@@ -0,0 +1,15 @@
+/* ======================= Declaración de variables ========================= */
+var myFirstVariable;
+myFirstVariable = "Esto es un string (cadena de texto)";
+
+var secondVariable = "Se puede asignar un valor en la misma declaración";
+
+/* ======================= Forma moderna ========================= */
+
+let variable3;
+
+let variable4 = "Igual que con var";
+
+variable3 = 3;
+
+const thisIsAConstant = "Valor inmutable";
