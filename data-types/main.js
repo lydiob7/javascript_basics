4f7be32c8f85ec080100797162f4bbb955c2951a
Tomi Scattini <tomiscattini@Tomis-Mac-mini.local>
2026-01-09 09:55:06 +0100

    Feature: add variables in data-types

--- data-types/main.js
+++ data-types/main.js
@@ -11,7 +11,23 @@ const myNull = null;
 
 const myUndefined = undefined;
 
-const stringWithSingleQuotes = "Se pueden usar comillas simples";
+const stringWithSingleQuotes = 'Se pueden usar comillas simples';
 const combinedQuotes =
   "Puedo usar comillas simples ' dentro de comillas dobles y al reves. También puedo usar \" escapando con una barra invertida";
 const anotherString = `También "backticks", esto permite user variables dentro de la cadena de texto como: ${myString}`;
+const multilineString = `
+Praesent quis orci sit amet ante facilisis suscipit. Integer in eros molestie, ultricies arcu ac, cursus quam. Nulla facilisi. Ut egestas semper magna ac condimentum. Aliquam erat volutpat. Sed bibendum sollicitudin orci, at viverra metus vehicula sed.
+
+Morbi molestie arcu sit amet libero porttitor, a mollis odio suscipit. Fusce at sapien id justo cursus mollis. Ut non orci in magna pretium consequat. Nam id purus eu velit vulputate elementum. Mauris ac sapien non felis scelerisque tincidunt.
+
+Nullam nec turpis et arcu egestas commodo. Integer sit amet metus non tortor tincidunt interdum. Donec et metus mollis, ultricies est at, ultricies nulla. Morbi non libero magna. Praesent imperdiet magna ac ipsum cursus, ut fermentum turpis tincidunt.
+
+Nullam quis arcu in magna pulvinar tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nulla ut cursus laoreet. Nullam elementum lorem vel facilisis laoreet. Cras ac turpis vel erat vehicula venenatis.
+
+Sed at risus vel nulla consequat fermentum. Donec et orci mauris. Nullam tempor velit id mi luctus, a scelerisque libero accumsan. In hac habitasse platea dictumst. Cras ac nunc nec massa tristique fringilla.
+`;
+
+const myBigint = 90194012394501950n;
+const mySymbol = Symbol("simbolo");
+
+/* ======================= Datos complejos ========================= */
