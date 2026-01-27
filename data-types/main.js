ceeed4b8bbbdfadf13a06840be81a8aa7be7242e
Tomi Scattini <tomiscattini@Tomis-Mac-mini.local>
2026-01-27 14:20:29 +0100

    Update: tipos de datos js

--- data-types/main.js
+++ data-types/main.js
@@ -1,41 +1,32 @@
 /* ======================= Datos primitivos ========================= */
 
-const myNumber = 1;
-const decimalNumber = 0.8419481;
+const miNumero = 89;
+const miCadenaDeTexto = "Esto es un string";
+const otroString = "Esto tambien es valido";
+const tercerString = `Esto tambien`;
+const miBooleano = true;
+const miNull = null;
+const miUndefined = undefined;
+const miBigInt = 9089419023840912873490n;
+const miSimbolo = Symbol();
 
-const myString = "Texto plano";
+/* ======================= Datos complejos ========================= */
 
-const myBoolean = true;
-
-const myNull = null;
-
-const myUndefined = undefined;
-
-const stringWithSingleQuotes = 'Se pueden usar comillas simples';
-const combinedQuotes =
-  "Puedo usar 'comillas simples'  dentro de comillas dobles y al reves. También puedo usar \" escapando con una barra invertida";
-const anotherString = `También "backticks", esto permite user variables dentro de la cadena de texto como: ${myString}`;
-const multilineString = `
-Praesent quis orci sit amet ante facilisis suscipit. Integer in eros molestie, ultricies arcu ac, cursus quam. Nulla facilisi. Ut egestas semper magna ac condimentum. Aliquam erat volutpat. Sed bibendum sollicitudin orci, at viverra metus vehicula sed.
-
-Morbi molestie arcu sit amet libero porttitor, a mollis odio suscipit. Fusce at sapien id justo cursus mollis. Ut non orci in magna pretium consequat. Nam id purus eu velit vulputate elementum. Mauris ac sapien non felis scelerisque tincidunt.
-
-Nullam nec turpis et arcu egestas commodo. Integer sit amet metus non tortor tincidunt interdum. Donec et metus mollis, ultricies est at, ultricies nulla. Morbi non libero magna. Praesent imperdiet magna ac ipsum cursus, ut fermentum turpis tincidunt.
-
-Nullam quis arcu in magna pulvinar tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nulla ut cursus laoreet. Nullam elementum lorem vel facilisis laoreet. Cras ac turpis vel erat vehicula venenatis.
-
-Sed at risus vel nulla consequat fermentum. Donec et orci mauris. Nullam tempor velit id mi luctus, a scelerisque libero accumsan. In hac habitasse platea dictumst. Cras ac nunc nec massa tristique fringilla.
-`;
-
-const myBigint = 90194012394501950n;
-const mySymbol = Symbol("simbolo");
-
-/* ======================= Objetos ========================= */
-
-const myObject = {
-  name: "Nombre",
-  type: "Tipo",
-  num: 12,
+const miObjeto = {
+  color: "Rojo",
+  altura: 88,
+  ancho: 12,
+  direccion: {
+    calle: "algo",
+    numer: 123,
+  },
 };
 
-const myArray = [1, 2, 3, 4];
+const miArreglo = [1, 2, 3, 4, "Hola", { a: "b" }, miObjeto];
+
+/* ======================= Strings (cadenas de texto) ========================= */
+
+const cadenaDeTexto = 'Utilizar una frase "Esta es la frase" ';
+const nombre = "Juan";
+const apellido = "Perex";
+const descripcion = `El nombre de esta persona es: ${nombre} ${apellido}`;
